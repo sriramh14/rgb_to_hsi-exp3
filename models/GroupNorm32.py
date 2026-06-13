fdf8d33448e58f7f2d6c629595a306edc4e4ad10
sriramh14 <sriramhari1014@gmail.com>
2026-06-13 12:47:52 +0530

    Create GroupNorm32.py

---
+++ models/GroupNorm32.py
@@ -0,0 +1,7 @@
+class GroupNorm32(nn.GroupNorm):
+    """GroupNorm that chooses a valid number of groups."""
+    def __init__(self, channels: int):
+        groups = min(32, channels)
+        while channels % groups != 0:
+            groups -= 1
+        super().__init__(groups, channels)
