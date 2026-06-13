ed80f2ef0d778b30a00b6a6b13cda8eb200da16b
sriramh14 <sriramhari1014@gmail.com>
2026-06-13 13:12:04 +0530

    Add files via upload

---
+++ loss/__init__.py
@@ -0,0 +1,15 @@
+"""Loss functions for RGB-to-HSI reconstruction."""
+
+from .mrae_loss import mrae_loss
+from .rgb_consistency_loss import rgb_consistency_loss
+from .sam_loss import sam_loss
+from .spectral_gradient_loss import spectral_gradient_loss
+from .spectral_smoothness_loss import spectral_smoothness_loss
+
+__all__ = [
+    "mrae_loss",
+    "rgb_consistency_loss",
+    "sam_loss",
+    "spectral_gradient_loss",
+    "spectral_smoothness_loss",
+]
