8b41f8b31c7f6560fb3beefa4d8d4bc1a37bb60c
sriramh14 <sriramhari1014@gmail.com>
2026-06-13 15:35:00 +0530

    Update ResBlock.py

--- models/ResBlock.py
+++ models/ResBlock.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from .GroupNorm32 import GroupNorm32
 class ResBlock(nn.Module):
     """Residual block with timestep conditioning."""
