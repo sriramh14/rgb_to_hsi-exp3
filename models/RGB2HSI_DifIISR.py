7008ed86d2c67228e2312cdf3005ed2beef47fdd
sriramh14 <sriramhari1014@gmail.com>
2026-06-13 15:36:13 +0530

    Update RGB2HSI_DifIISR.py

--- models/RGB2HSI_DifIISR.py
+++ models/RGB2HSI_DifIISR.py
@@ -5,7 +5,7 @@ from .CoarseHSINet import CoarseHSINet
 from .HSIResidualDiffusionUNet import HSIResidualDiffusionUNet
 from typing import Optional
 from .Diffusion_scheduler import extract_to_shape, make_eta_schedule
-
+from loss import *
 class RGB2HSI_DifIISR(nn.Module):
     """
     RGB-to-HSI model adapted from DifIISR's residual-shift diffusion idea.
